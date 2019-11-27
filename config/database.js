563db879b4036a7dcf11dc0576e0d1473440c3ca
Thalles Gremi <thallesgremi@gmail.com>
2019-11-27 17:07:57 -0300

    Docker containers.

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const getConnection = () => {
 	console.log('createConnection on DB');
-	mongoose.connect('mongodb://localhost:27017/jobTest', { useNewUrlParser: true });
+	mongoose.connect('mongodb://mongo:27017/jobTest', { useNewUrlParser: true });
 	const db = mongoose.connection;
 	db.on('error', console.error.bind(console, 'connection error:'));
 };
